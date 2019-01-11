4e27b624b3c81db914a67058e13c3e09926b36eb
Josh <josh@my.prideschools.org>
2019-01-11 10:13:31 -0800

    Buttons to Info Page on all pages

---
+++ Domestic Violence Pizza App/Domestic Violence Pizza App/StartingPage.swift
@@ -0,0 +1,30 @@
+//
+//  StartingPage.swift
+//  Domestic Violence Pizza App
+//
+//  Created by Josh Admin on 1/11/19.
+//  Copyright © 2019 Josh Admin. All rights reserved.
+//
+
+import UIKit
+
+class StartingPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
