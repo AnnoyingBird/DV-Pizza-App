8a7ee6f434bc06b4a527caa084f372aeb313e19e
Tucker Boniface <tucker@boniface.tech>
2019-01-28 14:19:26 -0800

    remove tests and placeholder comments

--- Domestic Violence Pizza App/Domestic Violence Pizza App/Toppings.swift
+++ Domestic Violence Pizza App/Domestic Violence Pizza App/Toppings.swift
@@ -1,30 +1,12 @@
-//
 //  Toppings.swift
 //  Domestic Violence Pizza App
-//
-//  Created by Kennadi Admin on 1/28/19.
-//  Copyright © 2019 Josh Admin. All rights reserved.
-//
 
 import UIKit
 
 class Toppings: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
