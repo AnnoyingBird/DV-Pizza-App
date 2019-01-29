eca445fff7dc8151d20f9fc62071a3dcde43227e
K.A <kennadi@my.prideprepschool.org>
2019-01-28 19:29:16 -0800

    Kandi Doing Things Pt. I
    Linked buttons, edited some small things.

--- Domestic Violence Pizza App/Domestic Violence Pizza App/SaucesDrinks.swift
+++ Domestic Violence Pizza App/Domestic Violence Pizza App/SaucesDrinks.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class SaucesDrinks: UIViewController {
 
+    @IBAction func RedS(_ sender: Any) {
+    }
+    @IBAction func WhiteS(_ sender: Any) {
+    }
+    @IBAction func NoS(_ sender: Any) {
+    }
+    @IBAction func ColaD(_ sender: Any) {
+    }
+    
+    @IBAction func OrangeD(_ sender: Any) {
+    }
+    @IBAction func RootD(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
--- Domestic Violence Pizza App/Domestic Violence Pizza App/Toppings.swift
+++ Domestic Violence Pizza App/Domestic Violence Pizza App/Toppings.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class Toppings: UIViewController {
-
+    @IBAction func Peppernoi(_ sender: Any) {
+    }
+    @IBAction func Mild(_ sender: Any) {
+    }
+    @IBAction func Spicy(_ sender: Any) {
+    }
+    @IBAction func Canadian(_ sender: Any) {
+    }
+    @IBAction func Bacon(_ sender: Any) {
+    }
+    @IBAction func Olive(_ sender: Any) {
+    }
+    @IBAction func BellPepps(_ sender: Any) {
+    }
+    @IBAction func Mushrooms(_ sender: Any) {
+    }
+    @IBAction func Pineapple(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
